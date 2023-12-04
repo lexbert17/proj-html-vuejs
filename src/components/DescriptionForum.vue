2f701947ca269eeaf71f63eabd305f3b83a71856
lexbert17 <lexbertlazo@gmail.com>
2023-12-04 20:01:55 +0100

    add: contenuto DescriptionForum

--- src/components/DescriptionForum.vue
+++ src/components/DescriptionForum.vue
@@ -9,9 +9,24 @@ export default{
 </script>
 
 <template>
- 
+<section>
+    <div class="container ms_small">
+      <h4>AVADA FORUM IS THE PLACE TO BE</h4>
+      <p>In quins lectus  sed leo elementum faucibus in dapibus dictum.
+        Nulla molestie tortor nec lectus venenatis, sed blandit dui finibus.
+        dolor at, bibendum purus. Donec vehicula nec tortor ac finibus.
+      </p>
+    </div>
+</section>
 </template>
 
 <style lang="scss" scoped>
+section{
 
+  .ms_small{
+    text-align: center;
+    width: 50%;
+    padding: 3rem 0;
+  }
+}
 </style>
