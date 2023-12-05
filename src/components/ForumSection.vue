d052899ed8b143112c76557a9a8fe75dec1516f7
lexbert17 <lexbertlazo@gmail.com>
2023-12-05 21:38:26 +0100

    add: contenutoo ForumSection

--- src/components/ForumSection.vue
+++ src/components/ForumSection.vue
@@ -1,17 +1,113 @@
-<script >
+   <script >
+  import { store } from '../store';
 export default{
   data() {
     return{
-      
+      store,
     }
   }
 }
 </script>
 
 <template>
+  <section>
+      <div class="ms_container container">
+        <h4>PHASELLUS EGET METUS</h4>
+        <h2>Forum Sections</h2>
+        <hr class="mt-4 mb-4">
+        <div class="row d-flex ">
+          <div class="col-4 d-flex gap-5 ms_col">
+
+            <div class="ms_card" v-for="forum in store.arrayForum" >
+                <div class="content">
+                  <a href="">
+                    <div class="orange">
+                      <i :class="forum.icon" style="color: #ffffff;"></i>
+                    </div>
+                    <h6 class="card-title mt-3">{{ forum.title }}</h6>   
+                  </a>
+                </div>
+             </div>
+
+            
+
+            
+
+       
   
+
+         
+
+
+
+
+
+          </div>
+        </div>
+      </div>
+  </section>
 </template>
 
 <style lang="scss" scoped>
+section{
+text-align: center;
+  .ms_container{
+    width: 75%;
+    background: palegreen;
 
+      h4{}
+
+      h2{
+
+      }
+      hr{
+      width: 70px;
+      margin: auto;
+      border: 1.5px solid brown;
+      
+      }
+      .row{
+        width: 100%;
+        margin: auto;
+        .ms_col{
+          flex-wrap: wrap;
+          justify-content: space-around;
+          width: 100%;
+          
+
+        }
+      }
+    .ms_card{
+      background-color: green;
+      aspect-ratio: 1;
+      width: 200px;
+
+      .content{
+        display: flex;
+        justify-content: center;
+       align-items: center;
+       height: 100%;
+
+       a{
+        text-decoration: none;
+        color: black;
+          .orange{
+              background-color: orange;
+              display: inline-block;
+              height: 4rem;
+              aspect-ratio: 1;
+              border-radius: 50%;
+              
+
+              i{
+                position: relative;
+                top: 25%;
+                transform: translateY(-50%);
+              }
+          }
+       }  
+      }  
+    }
+  }
+}
 </style>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,39 @@
 import { reactive } from "vue";
 export const store = reactive ({
     arrayHeader:["Home" , "Apple", "Microsoft", "Android", "Forums", "Contact us", "join us"],
-    sentence:"READ OUR BLOG"
+    sentence:"READ OUR BLOG",
+    arrayForum:[
+        {
+            icon:"fa-solid fa-file-lines fa-2xl",
+            title:"News & Community"
+        },
+        {
+            icon:"fa-brands fa-apple fa-2xl",
+            title:"Apple Forum"
+        },
+        {
+            icon:"fa-brands fa-windows fa-2xl",
+            title:"Microsoft Forum"
+        },
+        {
+            icon:"fa-brands fa-android fa-2xl",
+            title:"Android Forum"
+        },
+        {
+            icon:"fa-solid fa-mug-saucer fa-2xl",
+            title:"General Discussion"
+        },
+        {
+            icon:"fa-solid fa-cubes fa-2xl" ,
+            title:"Apps & Software"
+        },
+        {
+            icon:"fa-solid fa-briefcase fa-2xl",
+            title:"Gadgets & Stuff"
+        },
+        {
+            icon:"fa-solid fa-wrench fa-2xl",
+            title:"Tutorials & Guides"
+        },
+    ]
 })
